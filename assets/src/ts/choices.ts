e2736fc55a29139de1e987172a25f5b7bb0e756d
Tiago Joao <tiago.joao@esirion.de>
2024-11-11 13:41:25 +0100

    fix(admin): choices dropdown width

--- assets/src/ts/choices.ts
+++ assets/src/ts/choices.ts
@@ -1,7 +1,7 @@
 import Choices from "choices.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-	const oeb_users = document.getElementById('#oeb_users');
+	const oeb_users = document.getElementById('oeb_users');
 	if (oeb_users) {
 		new Choices(oeb_users, {
 			removeItemButton: true,
